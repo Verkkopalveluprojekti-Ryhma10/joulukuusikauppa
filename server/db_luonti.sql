b97374bbc6e8d20ab684883add33edfdb58c7a88
Ossi Kauppi <ossi.kauppi@gmail.com>
2023-11-13 20:47:03 +0200

    Update db_luonti.sql

--- server/db_luonti.sql
+++ server/db_luonti.sql
@@ -1,23 +1,18 @@
--- Luodaan tietokanta
-CREATE DATABASE IF NOT EXISTS joulukuusikauppa;
-
--- Luodaan taulu tuotekategorioille
 CREATE TABLE `product_categories` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(30) NOT NULL
 );
 
--- Luodaan taulu tuotteille
 CREATE TABLE `products` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(30) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
   `category` INT(2) NOT NULL,
   `price` DECIMAL(3,2) NOT NULL,
-  `storage` INT(5) NOT NULL
+  `storage` INT(5) NOT NULL,
+  `image_url` TEXT NOT NULL
 );
 
--- Luodaan taulu asiakkaille
 CREATE TABLE `customers` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(60) NOT NULL,
@@ -29,19 +24,16 @@ CREATE TABLE `customers` (
   `registeredAt` DATE DEFAULT (CURRENT_TIMESTAMP)
 );
 
--- Luodaan taulu tilauksille
 CREATE TABLE `orders` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `customer` INT NOT NULL,
   `payStatus` VARCHAR(5) NOT NULL,
   `orderStatus` VARCHAR(5) NOT NULL,
-  `deliveryStatus` VARCHAR(5) NOT NULL,
   `createdAt` DATE DEFAULT (CURRENT_TIMESTAMP),
   `modifiedAt` DATE DEFAULT (CURRENT_TIMESTAMP)
 );
 
--- Luodaan taulu tilausriveille
-CREATE TABLE `order_items` (
+CREATE TABLE `orderItems` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `order` INT NOT NULL,
   `product` INT NOT NULL,
@@ -49,14 +41,10 @@ CREATE TABLE `order_items` (
   `price` DECIMAL(5,2) NOT NULL
 );
 
--- Luodaan relaatio tuote -> tuotekategoria
 ALTER TABLE `products` ADD CONSTRAINT `productCategoryID` FOREIGN KEY (`category`) REFERENCES `product_categories` (`id`);
 
--- Luodaan relaatio tilaus -> asiakas
 ALTER TABLE `orders` ADD CONSTRAINT `customerID` FOREIGN KEY (`customer`) REFERENCES `customers` (`id`);
 
--- Luodaan relaatio tilausrivi -> tilaus
-ALTER TABLE `order_items` ADD CONSTRAINT `orderID` FOREIGN KEY (`order`) REFERENCES `orders` (`id`);
+ALTER TABLE `orderItems` ADD CONSTRAINT `orderID` FOREIGN KEY (`order`) REFERENCES `orders` (`id`);
 
--- Luodaan relaatio tilausrivi -> tuote
-ALTER TABLE `order_items` ADD CONSTRAINT `productID` FOREIGN KEY (`product`) REFERENCES `products` (`id`);
+ALTER TABLE `orderItems` ADD CONSTRAINT `productID` FOREIGN KEY (`product`) REFERENCES `products` (`id`);
