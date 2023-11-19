2a0dee35885c04216744c8226c178a5e1c469a82
Ossi Kauppi <ossi.kauppi@gmail.com>
2023-11-19 22:35:05 +0200

    Lisätty backendin toimintoja

--- server/index.js
+++ server/index.js
@@ -7,20 +7,66 @@ const port = 3001
 
 const app = express()
 
-
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get("/", async (req,res) => {
+app.get('/', async (req,res) => {
     try {
         const connection = await mysql.createConnection(config.db)
-        res.status(200).json({message: "Node server is running on port " + port})
+        res.status(200).json({message: 'Node server is running on port ' + port})
     } catch (err) {
         res.status(500).send(err.message)        
     }
+});
 
-})
+// Haetaan tuotekategoriat
+app.get('/categories', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(conf);
+        const subcategory = req.query.subcategory;
+
+        let result;        
+        if(subcategory){
+            result = await connection.execute("SELECT * FROM product_categories WHERE subcategory=?", [subcategory]);
+        }else{
+            result = await connection.execute("SELECT * FROM product_categories");
+        }
+        
+        //Tuloksen ensimmäinen indeksi sisältää tulokset taulukkona
+        res.json(result[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Haetaan tuotteet
+app.get('/products', async (req, res) => {
+    try {
+        // Koitetaan muodostaa tietokantayhteys
+        const connection = await mysql.createConnection(conf);
+        // Luetaan jos annettu parametreja
+        const category = req.query.category;
+        const subcategory = req.query.subcategory;
+
+        let result;     
+        if(category){
+            // Haetaan tuotteet jos kategoria määritetty
+            result = await connection.execute("SELECT * FROM products WHERE category=?", [category]);
+        }else if(subcategory){
+            // Haetaan tuotteen jos alikategoria määritetty
+            result = await connection.execute("SELECT * FROM products WHERE subcategory=?", [subcategory]);
+        } else {
+            // Haetaan kaikki tuotteet
+            result = await connection.execute("SELECT * FROM products");
+        }
+        
+        //Tuloksen ensimmäinen indeksi sisältää tulokset taulukkona
+        res.json(result[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 app.listen(port,() => {
     console.log(`Server is running on port ${port}`)
