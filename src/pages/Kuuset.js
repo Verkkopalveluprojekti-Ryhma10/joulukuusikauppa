4ca3037e6eb72fc75dbaca14f93b5f8743795e20
Ossi Kauppi <ossi.kauppi@gmail.com>
2023-11-20 19:27:11 +0200

    Kuuset ja Muuta pohja

---
+++ src/pages/Kuuset.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Kuuset = () => {
+    return (
+        <div>
+            <h1>Plaaplaa</h1>
+        </div>
+    );
+
+};
+
+export default Kuuset;
---
+++ src/pages/Muuta.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Muuta = () => {
+    return (
+        <div>
+            <h1>Plaaplaa</h1>
+        </div>
+    );
+
+};
+
+export default Muuta;
