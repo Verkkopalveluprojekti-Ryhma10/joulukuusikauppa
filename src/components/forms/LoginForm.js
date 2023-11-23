2811a8ad91065de42fdd68bd65e56e9e75dc72ef
MariellaHeikkila <mariella.heikkila@gmail.com>
2023-11-23 12:25:23 +0200

    added axios, preactsignals and loginform inputs

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,10 +1,28 @@
+import axios from "axios"
+import { useState } from "react"
 
+//npm i axios
+//npm i @preact/signals-react
 
 export default function LoginForm() {
 
+    const [uname, setUname] = useState('')
+    const [pw, setPw] = useState('')
+
+    function login() {
+        //send as parameters
+        axios.postForm('http://localhost:3001/login', {uname, pw})
+        .then()
+        .catch()
+
+    }
+
+
     return(
         <div>
-            <p>Kirjaudu</p>
+            <input value={uname} onChange={e => setUname(e.target.value)}/>
+            <input value={pw} onChange={e => setPw(e.target.value)}/>
+            <button onClick={login}>Kirjaudu</button>
         </div>
     )
 }
