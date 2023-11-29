1ea1189161acf36cb3539d7ad53b7a1bcf5fc155
MariellaHeikkila <mariella.heikkila@gmail.com>
2023-11-29 10:09:00 +0200

    update latvatahdet fetches by categories

--- src/pages/Latvatahdet.js
+++ src/pages/Latvatahdet.js
@@ -1,33 +1,31 @@
-import { effect, signal } from '@preact/signals-react';
 import axios from 'axios';
-import React from 'react';
-
-const latvaTahdetData = signal([])
-
-effect(()=>{
-
-    axios.get('http://localhost:3001/products', )
-
-    .then(res => latvaTahdetData.value = res.data)
-    .catch(error => console.log(error.message))
- }, [])
+import React, { useEffect, useState } from 'react';
+import { productData } from '../components/signals/ProductDataSignal';
+//import needed signal
 
 const Latvatahdet = () => {
 
-    //subcategory
-    console.log(latvaTahdetData.value);
+    //choose which category you need
+    const [subCategory, setSubCategory] = useState(3)
+    const [errormessage, setErrormessage] = useState('')
 
-
-
+    //add subcategory using params to get endpoint
+    //productData gets value from database
+    useEffect(() => {
+        axios.get('http://localhost:3001/products', {params: {category: subCategory}})
+        .then(res => productData.value = res.data)
+        .catch(error => setErrormessage('tapahtui virhe: ' + error.message))      
+    }, [])
+    
+    //mapped data presented
     return (
         <div>
             <h1>Tämä on Latvatähdet-sivu</h1>
-            {latvaTahdetData.value.map((item, index) =>
+            {productData.value.map((item, index) =>
             <div key={index}>
                 <li>
-                    {item.name}
+                   tuote: {item.name} , hinta: {item.price}
                 </li>
-
             </div>
             )}
         </div>
