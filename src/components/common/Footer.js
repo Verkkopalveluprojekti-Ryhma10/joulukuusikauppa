a5009c65fcaeb9fee37d562b96ebb8a2bccdb1e6
MariellaHeikkila <mariella.heikkila@gmail.com>
2023-12-04 14:50:27 +0200

    added fontawicons to footer + css

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,8 @@
 import '../../styles/Footer.css'
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
@@ -9,6 +12,17 @@ function Footer() {
                 <p><strong>Sähköposti:</strong> info@esimerkki.fi</p>
                 <p><strong>Puhelin:</strong> 0123-4567589</p>
                 <p><strong>Y-tunnus</strong> 1234-5678</p>
+            </div>
+            <div className="footer-content">
+            <Link to="https://www.instagram.com">
+                <FontAwesomeIcon icon={faInstagram} className='icons' size='2x'/>
+            </Link>
+            <Link to="https://www.facebook.com">
+                <FontAwesomeIcon icon={faFacebook} className='icons' size='2x'/>
+            </Link>
+            <Link to="https://www.twitter.com">
+                <FontAwesomeIcon icon={faTwitter} className='icons' size='2x'/>
+            </Link>
             </div>
         </footer>
     );
