3db3b1a7406cc31afffafea6e6e68b20b711191e
aino.mrtsknn@gmail.com <you@example.com>
2023-12-18 07:38:50 +0200

    searchbar removed

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -45,15 +45,6 @@ function Navb() {
               )})
             }
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
           <ShoppingCartDrop />
         </Navbar.Collapse>     
       </Container>
